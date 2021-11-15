4caa86cc540b21b5f20f808eb5861a54057fde25
Jamal <jmlkhon17@gmail.com>
2021-11-15 12:22:13 +0500

    handling post

--- app.js
+++ app.js
@@ -2,14 +2,32 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
+app.use(express.json());
+
 const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours.json'));
 
 app.get('/api/v1/tours', (req, res) => {
   res.json({
     status: 'success',
+    result: tours.length,
     data: {
       tours,
     },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours.length + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile('./dev-data/data/tours.json', JSON.stringify(tours), (err) => {
+    res.json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
   });
 });
 
