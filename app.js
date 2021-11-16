0a67c55b633db352baba63889a0174a62ad339bc
Jamal <jmlkhon17@gmail.com>
2021-11-16 10:58:36 +0500

    eslint config

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const userRouter = require('./route/userRouter');
 
 //middlewares
 app.use(express.json());
-app.use(morgan('dev'));
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // ROUTES
 app.use('/api/v1/tours', tourRouter);
