f32370f40b54696e8ddf76dd7ad8078dd40003d3
Jamal <jmlkhon17@gmail.com>
2021-11-17 13:35:36 +0500

    creating docs

--- controller/tourController.js
+++ controller/tourController.js
@@ -3,15 +3,7 @@ const Tour = require('./../models/tourModel');
 
 
 
-exports.checkBody = (req, res, next) => {
-  // if (!req.body.name || !req.body.price) {
-  //   return res.status(400).json({
-  //     status: 'bad request',
-  //     message: 'missing  name or price',
-  //   });
-  // }
-  next();
-};
+
 
 exports.getAllTours = (req, res) => {
   // res.json({
@@ -32,8 +24,21 @@ exports.getTour = (req, res) => {
   //   },
   // });
 };
-exports.createTour = (req, res) => {
-  
+exports.createTour = async (req, res) => {
+  try{
+    const newTour = await Tour.create(req.body);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    })
+  }catch(err){
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    })
+  }
 };
 exports.updateTour = (req, res) => {
   
