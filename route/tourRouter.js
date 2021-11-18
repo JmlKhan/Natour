5772b4b2e8f7d2860724369be3e28d2fba9ba932
Jamal <jmlkhon17@gmail.com>
2021-11-18 19:46:49 +0500

    aggregation pipline

--- route/tourRouter.js
+++ route/tourRouter.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 //router.param('id', tourController.checkID);
 router.route('/top-alias').get(tourController.topAlias, tourController.getAllTours)
+router.route('/tour-stats').get(tourController.getTourStats);
 router
   .route('/')
   .get(tourController.getAllTours)
