1f51d7fd3bff9c7411cf3a421719e28430b42b7f
Jamal <jmlkhon17@gmail.com>
2021-11-18 10:11:03 +0500

    sorting

--- controller/tourController.js
+++ controller/tourController.js
@@ -14,9 +14,17 @@ try{
   // 2.Advanced filtering
   let queryStr = JSON.stringify(queryObj);
   queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-  console.log(JSON.parse(queryStr))
+  
+  let query = Tour.find(JSON.parse(queryStr))
 
-  const query = await Tour.find(JSON.parse(queryStr))
+  //Sorting
+  if(req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    console.log(sortBy)
+    query = query.sort(req.query.sort);
+
+    //sort('price ratingsAverage')
+  }
 
   ////execute query
   const tours = await query;
