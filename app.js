7d9ac1fb14436c5a9b143b0b34fbda886c95045b
Jamal <jmlkhon17@gmail.com>
2021-11-21 15:34:57 +0500

    refactoring global error

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 
 const tourRouter = require('./route/tourRouter');
 const userRouter = require('./route/userRouter');
+const AppError = require('./utils/appError');
+const globalError = require('./controller/errorController');
 
 //middlewares
 app.use(express.json());
@@ -17,25 +19,9 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: 'Cant find url on this server'
-  // })
-
-  const err = new Error(`Cant find ${req.originalUrl} on this server`)
-  err.status = 'fail';
-  err.statusCode = 404;
-  next(err);
+  next( new AppError(`Can't find ${req.originalUrl} on this server`, 404) );
 })
 
-app.use( (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-})
+app.use(globalError)
 
 module.exports = app;
