dbcc74894dcb53171fd445f2b1d680bbed251da8
Jamal <jmlkhon17@gmail.com>
2021-11-23 17:47:29 +0500

    password reset

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -37,7 +38,9 @@ const userSchema = new mongoose.Schema({
             message: 'Password are not the same!'
         }
     },
-    passwordChangeAt: Date
+    passwordChangeAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date
 });
 
 userSchema.pre('save', async function(next) {
@@ -61,6 +64,21 @@ userSchema.methods.changesPasswordAfter = function(JWTTimestamp) {
     }
 
     return false;
+}
+
+userSchema.methods.createPasswordResetToken = function() {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+
+   this.passwordResetToken = crypto
+   .createHash('sha256')
+   .update(resetToken)
+   .digest('hex');
+
+   console.log({resetToken}, this.passwordResetToken);
+
+   this.passwordResetExpires  =Date.now() + 10 * 60 * 1000;
+
+   return resetToken;
 }
 
 const User = mongoose.model('User', userSchema);
