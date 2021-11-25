59a230c632762d385cfc68b4e34793d3ab860555
Jamal <jmlkhon17@gmail.com>
2021-11-25 15:07:14 +0500

    implementing rate limiting

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const tourRouter = require('./route/tourRouter');
 const userRouter = require('./route/userRouter');
 const AppError = require('./utils/appError');
 const globalError = require('./controller/errorController');
 
-//middlewares
+//global middlewares
 app.use(express.json());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this API! Please, try again after 15 minutes...'
+});
+app.use(limiter);
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
