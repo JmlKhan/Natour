de7d514e0c785e132e2696ae2833b977b37d4b76
Jamal <jmlkhon17@gmail.com>
2021-11-25 14:24:13 +0500

    sending jwt via cookie

--- models/userModel.js
+++ models/userModel.js
@@ -76,7 +76,7 @@ userSchema.methods.changesPasswordAfter = function(JWTTimestamp) {
     return false;
 };
 
-userSchema.pre(/^find/, function(next) {
+userSchema.pre('find', function(next) {
     //this points to the current query
 
     this.find({ active: {$ne: false} });
