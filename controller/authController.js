84c0dd9b7b9b48dc30cf4febb003ca38a8c82fe9
Jamal <jmlkhon17@gmail.com>
2021-11-25 09:52:40 +0500

    bug fix on updating user password

--- controller/authController.js
+++ controller/authController.js
@@ -151,7 +151,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
     createSendToken(user, 200, res);
 });
 
-exports.updatePassword = catchAsync ( async (req, res, next) => {
+exports.updatePassword = catchAsync( async(req, res, next) => {
     //1. Get userfrom collection
     const user = await User.findById(req.user.id).select('+password');
        
@@ -163,7 +163,8 @@ exports.updatePassword = catchAsync ( async (req, res, next) => {
     //3. if so, update password
     user.password = req.body.password;
     user.passwordConfirm = req.body.passwordConfirm;
-    await user.save({validateBeforeSave: false});
+    
+    await user.save();
 
     //4.log user in, sent JWT
     createSendToken(user, 200, res);
--- models/userModel.js
+++ models/userModel.js
@@ -50,11 +50,13 @@ userSchema.pre('save', async function(next) {
     this.password = await bcrypt.hash(this.password, 12);
     this.passwordConfirm = undefined;
     next();
-})
+});
 
 userSchema.pre('save',  function(next) {
     if(!this.isModified('password') || this.isNew) return next();
+    
     this.passwordChangeAt = Date.now() - 1000;
+    next();
 });
 userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
     return await bcrypt.compare(candidatePassword, userPassword)
