bc588122f6d715889238c47b83530460c701c6fd
Jamal <jmlkhon17@gmail.com>
2021-11-26 08:54:29 +0500

    modelling tour guides via embedded data model

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -81,6 +82,7 @@ const tourSchema = new mongoose.Schema({
           day: Number
       },   
     ],
+    guides: Array,
     imageCover: {
       type: String,
       required: [true,'A tour must have a cover image']
@@ -107,6 +109,13 @@ const tourSchema = new mongoose.Schema({
   //   console.log(this)
   //   next();
   // })
+
+  tourSchema.pre('save', async function(next) {
+    const guidesPromises = this.guides.map(async id => await User.findById(id));
+    this.guides = await Promise.all(guidesPromises);
+
+    next();
+  })
 
   //QUERY middleware
 
