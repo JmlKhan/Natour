034a10a3948a1f8752d9eba33d4b8adf7bda0415
Jamal <jmlkhon17@gmail.com>
2021-11-28 07:49:04 +0500

    improving reading performance with index

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './../../config.env' });
 const fs = require('fs');
 const Tour = require('./../../models/tourModel');
+const User = require('./../../models/userModel');
+const Review = require('./../../models/reviewModel');
 
 
 const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD
@@ -20,12 +22,15 @@ mongoose
 //Read file
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'));
 
 //import data into db
 
 const importData = async () => {
     try{
-        await Tour.create(tours);
+        await User.create(users, { validateBeforeSave: false });
+        await Review.create(reviews);
         console.log('Data successfully loaded')
     }catch(err){
         console.log(err)
@@ -37,6 +42,8 @@ const importData = async () => {
 const deleteData = async () => {
     try{
         await Tour.deleteMany();
+        await User.deleteMany();
+        await Review.deleteMany();
         console.log('Data successfully deleted')
         process.exit()
     }catch(err){
--- models/userModel.js
+++ models/userModel.js
@@ -48,55 +48,55 @@ const userSchema = new mongoose.Schema({
     passwordResetExpires: Date
 });
 
-userSchema.pre('save', async function(next) {
-    //only run this function if password was actually modified
-    if(!this.isModified('password')) return next();
-    //HASH the password with cost of 12
-    this.password = await bcrypt.hash(this.password, 12);
-    this.passwordConfirm = undefined;
-    next();
-});
+// userSchema.pre('save', async function(next) {
+//     //only run this function if password was actually modified
+//     if(!this.isModified('password')) return next();
+//     //HASH the password with cost of 12
+//     this.password = await bcrypt.hash(this.password, 12);
+//     this.passwordConfirm = undefined;
+//     next();
+// });
 
-userSchema.pre('save',  function(next) {
-    if(!this.isModified('password') || this.isNew) return next();
+// userSchema.pre('save',  function(next) {
+//     if(!this.isModified('password') || this.isNew) return next();
     
-    this.passwordChangeAt = Date.now() - 1000;
-    next();
-});
-userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
-    return await bcrypt.compare(candidatePassword, userPassword)
-}
+//     this.passwordChangeAt = Date.now() - 1000;
+//     next();
+// });
+// userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
+//     return await bcrypt.compare(candidatePassword, userPassword)
+// }
 
-userSchema.methods.changesPasswordAfter = function(JWTTimestamp) {
-    if(this.passwordChangeAt) {
-        const changedTimestamp = parseInt(this.passwordChangeAt.getTime()/1000,10);
+// userSchema.methods.changesPasswordAfter = function(JWTTimestamp) {
+//     if(this.passwordChangeAt) {
+//         const changedTimestamp = parseInt(this.passwordChangeAt.getTime()/1000,10);
         
-        return JWTTimestamp < changedTimestamp;
-    }
-    return false;
-};
+//         return JWTTimestamp < changedTimestamp;
+//     }
+//     return false;
+// };
 
-userSchema.pre('find', function(next) {
-    //this points to the current query
+// userSchema.pre('find', function(next) {
+//     //this points to the current query
 
-    this.find({ active: {$ne: false} });
-    next();
-})
+//     this.find({ active: {$ne: false} });
+//     next();
+// })
 
-userSchema.methods.createPasswordResetToken = function() {
-    const resetToken = crypto.randomBytes(32).toString('hex');
+// userSchema.methods.createPasswordResetToken = function() {
+//     const resetToken = crypto.randomBytes(32).toString('hex');
 
-   this.passwordResetToken = crypto
-   .createHash('sha256')
-   .update(resetToken)
-   .digest('hex');
+//    this.passwordResetToken = crypto
+//    .createHash('sha256')
+//    .update(resetToken)
+//    .digest('hex');
 
-   console.log({resetToken}, this.passwordResetToken);
+//    console.log({resetToken}, this.passwordResetToken);
 
-   this.passwordResetExpires  =Date.now() + 10 * 60 * 1000;
+//    this.passwordResetExpires  =Date.now() + 10 * 60 * 1000;
 
-   return resetToken;
-}
+//    return resetToken;
+// }
 
 const User = mongoose.model('User', userSchema);
 
